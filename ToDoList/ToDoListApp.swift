b9930c6216f5b7511ca0e22688d0fe28236dbe99
Nayanp35 <prajapatinayan35@gmail.com>
2023-07-15 13:44:38 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nayan Prajapati on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
